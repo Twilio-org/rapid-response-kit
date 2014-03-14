3532e8adea1e8d10f67ef75ff1f51c3386f0380d
Shyamal Ruparel <shyamalruparel1991@gmail.com>
2014-03-13 22:03:56 -0500

    Added requests for parse keys during install

--- install.py
+++ install.py
@@ -10,10 +10,22 @@ print
 account_sid = raw_input('Twilio Account Sid: ')
 auth_token = raw_input('Twilio Auth Token: ')
 
+print
+print
+print colored.red(" --- Enter your Parse information below to use Optional Survey app --- ")
+print
+print
+
+parse_app_id = raw_input('Parse APP ID: ')
+parse_rest_key = raw_input('Parse Rest Key: ')
+
 config = """# Configuration Auto-generated during installation
 SECRET_KEY = {}
 TWILIO_ACCOUNT_SID = '{}'
-TWILIO_AUTH_TOKEN = '{}'""".format(repr(os.urandom(20)), account_sid, auth_token)
+TWILIO_AUTH_TOKEN = '{}'
+PARSE_APP_ID = '{}'
+PARSE_REST_KEY = '{}'""".format(repr(os.urandom(20)), account_sid, auth_token,parse_app_id,parse_rest_key)
+
 
 f = open('rapid_response_kit/utils/config.py', 'w')
 f.write(config)
