81f33b586cc3613db9707808f9d1fd312d9177cb
Milton Mazzarri <milmazz@gmail.com>
2014-03-18 22:27:38 -0600

    Added list of 'voice' and 'language' supported by 'Say' verb.

---
+++ rapid_response_kit/utils/voices.py
@@ -0,0 +1,141 @@
+BASIC_LANGUAGES = [
+    {
+        "value": "en",
+        "name": "English, United States",
+        "default": True
+    },
+    {
+        "value": "en-gb",
+        "name": "English, UK"
+    },
+    {
+        "value": "es",
+        "name": "Spanish"
+    },
+    {
+        "value": "fr",
+        "name": "French"
+    },
+    {
+        "value": "de",
+        "name": "German"
+    },
+    {
+        "value": "it",
+        "name": "Italian"
+    }
+]
+
+ADVANCED_LANGUAGES = [
+    {
+        "value": "da-DK",
+        "name": "Danish, Denmark"
+    },
+    {
+        "value": "de-DE",
+        "name": "German, Germany"
+    },
+    {
+        "value": "en-AU",
+        "name": "English, Australia"
+    },
+    {
+        "value": "en-CA",
+        "name": "English, Canada"
+    },
+    {
+        "value": "en-GB",
+        "name": "English, UK"
+    },
+    {
+        "value": "en-IN",
+        "name": "English, India"
+    },
+    {
+        "value": "en-US",
+        "name": "English, United States",
+        "default": True
+    },
+    {
+        "value": "ca-ES",
+        "name": "Catalan, Spain"
+    },
+    {
+        "value": "es-ES",
+        "name": "Spanish, Spain"
+    },
+    {
+        "value": "es-MX",
+        "name": "Spanish, Mexico"
+    },
+    {
+        "value": "fi-FI",
+        "name": "Finnish, Finland"
+    },
+    {
+        "value": "fr-CA",
+        "name": "French, Canada"
+    },
+    {
+        "value": "fr-FR",
+        "name": "French, France"
+    },
+    {
+        "value": "it-IT",
+        "name": "Italian, Italy"
+    },
+    {
+        "value": "ja-JP",
+        "name": "Japanese, Japan"
+    },
+    {
+        "value": "ko-KR",
+        "name": "Korean, Korea"
+    },
+    {
+        "value": "nb-NO",
+        "name": "Norwegian, Norway"
+    },
+    {
+        "value": "nl-NL",
+        "name": "Dutch, Netherlands"
+    },
+    {
+        "value": "pl-PL",
+        "name": "Polish-Poland"
+    },
+    {
+        "value": "pt-BR",
+        "name": "Portuguese, Brazil"
+    },
+    {
+        "value": "pt-PT",
+        "name": "Portuguese, Portugal"
+    },
+    {
+        "value": "ru-RU",
+        "name": "Russian, Russia"
+    },
+    {
+        "value": "sv-SE",
+        "name": "Swedish, Sweden"
+    },
+    {
+        "value": "zh-CN",
+        "name": "Chinese (Mandarin)"
+    },
+    {
+        "value": "zh-HK",
+        "name": "Chinese (Cantonese)"
+    },
+    {
+        "value": "zh-TW",
+        "name": "Chinese (Taiwanese Mandarin)"
+    }
+]
+
+VOICES = {
+    "man": BASIC_LANGUAGES,
+    "woman": BASIC_LANGUAGES,
+    "alice": ADVANCED_LANGUAGES
+}
