273ad5c255067e0f458c3e00982e56aea308fb6a
Paul Hallett <hello@phalt.co>
2014-10-14 13:50:56 +0100

    Handles ams or mms way better now

--- rapid_response_kit/tools/noticeboard.py
+++ rapid_response_kit/tools/noticeboard.py
@@ -44,37 +44,33 @@ def install(app):
 
     @app.route('/noticeboard', methods=['POST'])
     def do_noticeboard():
-        numbers = parse_numbers(request.form['numbers'])
-
-        url = "{0}noticeboard/post".format(request.url_root)
-
-        live_url = '{0}noticeboard/live/{1}'.format(
-            request.url_root,
-            request.form['twilio_number'])
-
-        body = request.form.get('message', '').replace('{URL}', live_url)
-
         client = twilio()
 
+        url = "{0}noticeboard/post".format(request.url_root)
         client.phone_numbers.update(request.form['twilio_number'],
                                     sms_url=url,
                                     sms_method='POST',
                                     friendly_name='[RRKit] Noticeboard')
 
         from_number = client.phone_numbers.get(request.form['twilio_number'])
+        live_url = '{0}noticeboard/live/{1}'.format(
+            request.url_root,
+            from_number.phone_number
+        )
+        numbers = parse_numbers(request.form['numbers'])
+        body = request.form.get('message', '').replace('{URL}', live_url)
+        media = None if request.form['media'] == u'' else request.form['media']
+
 
         for num in numbers:
-            try:
-                client.messages.create(
-                    to=num,
-                    from_=from_number.phone_number,
-                    body=body,
-                    media_url=request.form.get('media', None)
-                )
-                flash('Sent {0} the message'.format(num), 'success')
-            except:
-                flash('Failed to send {0} the message'.format(num), 'danger')
-                return redirect('/noticeboard')
+            client.messages.create(
+                to=num,
+                from_=from_number.phone_number,
+                body=body,
+                media_url=media
+            )
+            flash('Sent {0} the message'.format(num), 'success')
+
 
         return redirect('/noticeboard')
 
@@ -107,7 +103,7 @@ def install(app):
         pusher_key = app.config.get('PUSHER_KEY', '')
         client = twilio()
         try:
-            cleaned_number = parse_numbers(number)
+            cleaned_number = number
         except:
             flash('We did not receive a correct number', 'danger')
             return redirect('/noticeboard')
@@ -115,7 +111,7 @@ def install(app):
         # Build a list of messages to our number that has media attached
         msgs = []
         for m in client.messages.list(to=cleaned_number):
-            if m.num_media > 0:
+            if int(m.num_media) > 0:
                 msgs.append(m)
 
         '''
@@ -135,5 +131,6 @@ def install(app):
         return render_template(
             'noticeboard_live.html',
             pusher_key=pusher_key,
-            messages=msg_media_list
+            messages=msg_media_list,
+            number=number
         )
